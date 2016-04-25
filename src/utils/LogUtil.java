03b30c993fb157b93ef4871ba0bb5fa450ba7e31
Nitesh.Saxena <ankit0711@gmail.com>
2016-04-25 15:15:51 +0530

    Added Utils package with Logger Class

---
+++ src/utils/LogUtil.java
@@ -0,0 +1,44 @@
+package utils;
+
+import org.apache.log4j.Logger;
+
+public class LogUtil {
+	private static Logger logger = Logger.getLogger(LogUtil.class.getName());
+	
+	public static void startTestCase(String className,String testCaseID){
+		logger.info("=============================================================================================================================");
+		logger.info("Starting execution of test case: "+ testCaseID + " of Class: "+ className);
+		logger.info("=============================================================================================================================");
+	}
+	
+	public static void endTestCase() {
+        logger.info("==================================  ******** END OF TEST CASE ********  =====================================================");
+    }
+	
+	public static void errorMessageWithStackTrace(String testCaseID,String message, Throwable throwable) {
+        logger.error("ERROR EXECUTING TEST CASE ID " + testCaseID + "\t");
+        logger.error("===========================================================================================================================");
+        logger.error(message, throwable);
+    }
+	
+	public static void info(String message) {
+        logger.info(message);
+    }
+
+    public static void warn(String message) {
+        logger.warn(message);
+    }
+
+    public static void error(String message) {
+        logger.error(message);
+    }
+    
+    public static void fatal(String message) {
+        logger.fatal(message);
+    }
+
+    public static void debug(String message) {
+        logger.debug(message);
+    }   
+    
+}
