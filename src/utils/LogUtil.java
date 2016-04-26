fac3b74e8a64e5653e4b0c2c938430deb85fb244
Nitesh.Saxena <ankit0711@gmail.com>
2016-04-26 18:07:49 +0530

    -Added ExecuteShell class for executing shell commands. -Made changes in LogUtil class.

--- src/utils/LogUtil.java
+++ src/utils/LogUtil.java
@@ -1,23 +1,24 @@
 package utils;
 
+import org.apache.log4j.Appender;
 import org.apache.log4j.Logger;
+
 
 public class LogUtil {
 	private static Logger logger = Logger.getLogger(LogUtil.class.getName());
-	
+
 	public static void startTestCase(String className,String testCaseID){
 		logger.info("=============================================================================================================================");
 		logger.info("Starting execution of test case: "+ testCaseID + " of Class: "+ className);
-		logger.info("=============================================================================================================================");
 	}
 	
 	public static void endTestCase() {
         logger.info("==================================  ******** END OF TEST CASE ********  =====================================================");
     }
 	
-	public static void errorMessageWithStackTrace(String testCaseID,String message, Throwable throwable) {
-        logger.error("ERROR EXECUTING TEST CASE ID " + testCaseID + "\t");
-        logger.error("===========================================================================================================================");
+	public static void errorMessageWithStackTrace(String message, Throwable throwable) {
+        logger.error("ERROR EXECUTING TEST");
+        logger.error("=========================================TEST CASE FAILED===================================================================");
         logger.error(message, throwable);
     }
 	
@@ -40,5 +41,5 @@ public class LogUtil {
     public static void debug(String message) {
         logger.debug(message);
     }   
-    
+
 }
